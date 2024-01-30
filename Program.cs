2350201864fd4256838ef3ee6152c51476a12485
luan <contato.mluans@gmail.com>
2024-01-30 18:18:13 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        string[] nomes = new string[10];
+        int[] idades = new int[10];
+
+        
+        for (int i = 0; i < 10; i++)
+        {
+            // Solicita o nome do aluno
+            Console.WriteLine("Digite o nome do aluno {0}: ", i + 1);
+            nomes[i] = Console.ReadLine();
+
+            
+            Console.WriteLine("Digite a idade do aluno {0}: ", i + 1);
+            idades[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Imprime a lista de alunos
+        Console.WriteLine("Lista de alunos:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Nome: {0}, Idade: {1}", nomes[i], idades[i]);
+        }
+
+        Console.ReadLine();
+    }
+}
